e964f48d8a59090af9d1048cb3d43588d08904a5
Ricardo Pinheiro <47854257+aluno20182@users.noreply.github.com>
2019-04-17 16:06:11 +0100

    Criação dos Modelos de Base de Dados

---
+++ Reservas/Reservas/Models/Lugares.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Reservas.Models
+{
+    public class Reservas
+    {
+        public int ID { get; set; }
+
+        public string IdCliente { get; set; }
+
+        public string LocalDaReservas { get; set; }
+
+        public decimal ValorMulta { get; set; }
+
+        public DateTime DataDaReservas { get; set; }
+
+        //  *************************************
+        //  Criação das chaves Forasteiras
+        //  *************************************
+
+        //  FK para Viatura
+        [ForeignKey("Viatura")]  //Anotações são feitas sobre o objeto que está por baixo
+        public int ViaturaFK { get; set; }  //Base de Dados
+
+        public Viaturas Viatura { get; set; }   // C#
+
+        [ForeignKey("Tecnico")]  //Anotações são feitas sobre o objeto que está por baixo
+        public int TecnicoFK { get; set; }  //Base de Dados
+
+        public Agentes Agente { get; set; }   // C#
+
+        [ForeignKey("Cliente")]  //Anotações são feitas sobre o objeto que está por baixo
+        public int ClienteFK { get; set; }  //Base de Dados
+
+        public Clientes Cliente { get; set; }   // C#
+
+
+        //lista das multas associadas à viatura
+        public ICollection<Reservas> ListaDeReservas { get; set; }
+
+    }
+}
---
+++ Reservas/Reservas/Models/Tecnicos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Reservas.Models
+{
+    public class Tecnicos
+    {
+        public int ID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Fotografia { get; set; }
+
+
+        //***************************************
+        //Lista das multas associadas ao agente
+        //***************************************
+
+        //lista das multas associadas à viatura
+        public ICollection<Reservas> ListaDeReservas { get; set; }
+
+    }
+}
