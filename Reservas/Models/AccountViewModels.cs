fbd55e8c6cbec7499b1daff523d8abd9b6952e6d
Ricardo Valente Pinheiro <aluno20182@ipt.pt>
2019-07-07 15:05:31 +0100

    Tradução + Logo

--- Reservas/Models/AccountViewModels.cs
+++ Reservas/Models/AccountViewModels.cs
@@ -76,7 +76,7 @@ namespace Reservas.Models
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Confirmar Password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
--- Reservas/Startup.cs
+++ Reservas/Startup.cs
@@ -30,32 +30,32 @@ namespace Reservas
             var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
 
             // criar a Role 'Tecnicos'
-            if (!roleManager.RoleExists("Tecnicos"))
+            if (!roleManager.RoleExists("Tecnico"))
             {
                 // não existe a 'role'
                 // então, criar essa role
                 var role = new IdentityRole();
-                role.Name = "Tecnicos";
+                role.Name = "Tecnico";
                 roleManager.Create(role);
             }
 
             // criar a Role 'RecursosHumanos'
-            if (!roleManager.RoleExists("RecursosHumanos"))
+            if (!roleManager.RoleExists("Recursos Humanos"))
             {
                 // não existe a 'role'
                 // então, criar essa role
                 var role = new IdentityRole();
-                role.Name = "RecursosHumanos";
+                role.Name = "Recursos Humanos";
                 roleManager.Create(role);
             }
 
             // criar a Role 'GestorReservas'
-            if (!roleManager.RoleExists("GestorReservas"))
+            if (!roleManager.RoleExists("Gestor Reservas"))
             {
                 // não existe a 'role'
                 // então, criar essa role
                 var role = new IdentityRole();
-                role.Name = "GestorReservas";
+                role.Name = "Gestor Reservas";
                 roleManager.Create(role);
             }
 
@@ -71,7 +71,7 @@ namespace Reservas
             //Adicionar o Utilizador à respetiva Role-Agente-
             if (chkUser.Succeeded)
             {
-                var result1 = userManager.AddToRole(user.Id, "Tecnicos");
+                var result1 = userManager.AddToRole(user.Id, "Tecnico");
             }
         }
 
